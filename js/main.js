e53095cae20e68f6e18b06830f224d219e10493a
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-17 15:23:43 +0200

    Aggiunta struttura js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nomeUtente = prompt ("Quale è il suo nome?");
+const cognomeUtente = prompt ("Quale è il suo cognome?");
+const colorePreferito = prompt("Quale è il suo colore preferito?");
+
+alert(`Salve sig. ${nomeUtente} ${cognomeUtente} ${colorePreferito} 22.`);
+const spanNomeCognomeColore = document.getElementById(`nome-cognome-colore`);
+
+spanNomeCognomeColore.innerHTML = `<strong class="text-danger">
+                              ${nomeUtente} ${cognomeUtente}
+                            </strong><br>                             <small>ti piace ${colorePreferito} come colore 22.</small>`;
+console.dir ("spanNomeCognomeColore");
