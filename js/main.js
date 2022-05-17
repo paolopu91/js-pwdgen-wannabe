06644fc89ed4e921cc5d54ccd6118f5571cac48c
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-17 17:18:40 +0200

    password generetor

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 const nomeUtente = prompt ("Quale è il suo nome?");
 const cognomeUtente = prompt ("Quale è il suo cognome?");
 const colorePreferito = prompt("Quale è il suo colore preferito?");
+const divContainer = document.getElementById ("password")
+messaggio = (`${nomeUtente}${cognomeUtente}${colorePreferito}22`)
+password.innerHTML = "<h1>Password Generata :"+messaggio+"</h1>";
 
-alert(`Salve sig. ${nomeUtente} ${cognomeUtente} ${colorePreferito} 22.`);
+// Questo è come avevo capito che andasse fatto l'esercizio
+
+// alert(`Salve sig. ${nomeUtente} ${cognomeUtente} ${colorePreferito} 22.`);
 // const nomeUtente = "Mattia"
 // const cognomeUtente = "Folcarelli"
 // const colorePreferito = "verde" 
-const spanNomeCognomeColore = document.getElementById(`nome-cognome-colore`);
 
-spanNomeCognomeColore.innerHTML =`<strong class="text-primary">
-                              ${nomeUtente} ${cognomeUtente}
-                            </strong>                             
-                            <small>ti piace ${colorePreferito} come colore 22.</small>`;
-console.log(spanNomeCognomeColore);
+
+// const spanNomeCognomeColore = document.getElementById(`nome-cognome-colore`);
+
+// spanNomeCognomeColore.innerHTML =`<strong class="text-primary">
+//                               ${nomeUtente} ${cognomeUtente}
+//                             </strong>                             
+//                             <small>ti piace ${colorePreferito} come colore 22.</small>`;
+// console.log(spanNomeCognomeColore);
